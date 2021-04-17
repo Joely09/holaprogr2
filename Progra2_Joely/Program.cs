dd18cf2ebbff541914515517b9c688c2bdc5b682
Joely09 <82679985+Joely09@users.noreply.github.com>
2021-04-16 21:42:49 -0400

    Add files via upload

---
+++ Progra2_Joely/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Progra2_Joely
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Progr2!");
+        }
+    }
+}
